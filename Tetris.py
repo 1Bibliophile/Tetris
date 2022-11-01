18fc2aae135012ee1848bdbbbea41fca544d5926
David Morgenstern <DavidtheConfident@gmail.com>
2022-10-31 22:47:21 -0500

    Moves Pieces according to keystrokes

--- Tetris.py
+++ Tetris.py
@@ -158,28 +158,18 @@ def check_lost(positions):
     pass
  
 def get_shape():
-    return random.choice(shapes)
+    return Piece(5, 0, random.choice(shapes))
  
  
 def draw_text_middle(text, size, color, surface):
     pass
    
 def draw_grid(surface, grid, row, col):
-    surface.fill((0,0,0))
-
-    pygame.font.init()
-    font = pygame.font.SysFont('comicsans', 60)
-    label = font.render('Tetris', 1, (255, 255, 255))
-
-    surface.blit(label, (top_left_x + (play_width / 2) - (label.get_width() / 2)), 30)
-
-    for i in label(len(grid)):
+    for i in range(len(grid)):
         for j in range(len(grid[i])):
             pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size, block_size), 0)
 
     pygame.draw.rect(surface, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 4)
-
-    pygame.display.update()
 
  
 def clear_rows(grid, locked):
@@ -188,12 +178,45 @@ def clear_rows(grid, locked):
 def draw_next_shape(shape, surface):
     pass
  
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+    surface.fill((0,0,0))
+
+    pygame.font.init()
+    font = pygame.font.SysFont('comicsans', 60)
+    label = font.render('Tetris', 1, (255, 255, 255))
+
+    surface.blit(label, (top_left_x + (play_width / 2) - (label.get_width() / 2)), 30)
+
+    draw_grid(surface, grid)
+
+    pygame.display.update()
  
 def main():
-    pass
- 
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run == False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1 
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+
 def main_menu():
     pass
  
