bd69c87604ed0241cad481e381bfe7fae8caa0d4
David Morgenstern <DavidtheConfident@gmail.com>
2022-11-24 19:34:18 -0600

    Checks if Shape is at Bottom of Screen

--- Tetris.py
+++ Tetris.py
@@ -164,10 +164,24 @@ def convert_shape_format(shape):
         positions[i] = (pos[0] - 2, pos[1] - 4) 
 
 def valid_space(shape, grid):
-    pass
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub]
+
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+    return True
+            
   
 def check_lost(positions):
-    pass
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+    return False
  
 def get_shape():
     return Piece(5, 0, random.choice(shapes))
