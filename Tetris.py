1899317c3b4257613b61fd16da687a16e08f8a9e
David Morgenstern <DavidtheConfident@gmail.com>
2022-11-26 19:37:38 -0600

    Displays YOU LOST on screen

--- Tetris.py
+++ Tetris.py
@@ -190,7 +190,10 @@ def get_shape():
  
  
 def draw_text_middle(text, size, color, surface):
-    pass
+    font = pygame.font.SysFont("comicsans", size, bold="True")
+    label = font.render(text, 1, color)
+
+    surface.blit(label, (top_left_x + play_width/2 - (label.get_width()/2), top_left_y + play_height/2 - label.get_height()/2))
    
 def draw_grid(surface, grid, row, col):
     sx = top_left_x
@@ -222,6 +225,8 @@ def clear_rows(grid, locked):
             if y < ind: 
                 newKey = (x, y + inc)
                 locked[newKey] = locked.pop(key)
+            
+    return inc 
 
  
 def draw_next_shape(shape, surface):
@@ -238,16 +243,22 @@ def draw_next_shape(shape, surface):
             if column == '0':
                 pygame.draw.rect(surface, shape.color, (sx + j*block_size, sy + i*block_size, block_size, block_size, 0))
 
-    surface.blit(label, (sx + 10, sy - 30))
+    surface.blit(label, (sx + 20, sy - 160))
  
-def draw_window(surface, grid):
+def draw_window(surface, grid, score=0):
     surface.fill((0,0,0))
 
     pygame.font.init()
     font = pygame.font.SysFont('comicsans', 60)
     label = font.render('Tetris', 1, (255, 255, 255))
 
-    surface.blit(label, (top_left_x + (play_width / 2) - (label.get_width() / 2)), 30)
+    surface.blit(label, (top_left_x + (play_width / 2) - (label.get_width() / 2), 30))
+
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Score: ' + str(score), 1, (255,255,255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -269,11 +280,20 @@ def main(win):
     clock = pygame.time.Clock()
     fall_time = 0
     fall_speed = 0.27
+    level_time = 0
+    score = 0
 
     while run:
         grid = create_grid(locked_positions)
         fall_time += clock.get_rawtime()
+        level_time += clock.get_rawtime()
         clock.tick()
+
+        if level_time/1000 > 5:
+            level_time = 0
+            if fall_speed > 0.12:
+                fall_speed -= 0.005
+
 
         if fall_time/1000 > fall_speed:
             fall_time = 0
@@ -321,11 +341,18 @@ def main(win):
             next_piece = get_shape()
             change_piece = False
 
+            score += clear_rows(grid, locked_positions) * 10 
+
+
+
         draw_window(win, grid)
         draw_next_shape(next_piece, win)
         pygame.display.update()
 
         if check_lost(locked_positions):
+            draw_text_middle(win, "YOU LOST!", 80, (255,255,255))
+            pygame.display.update()
+            pygame.time.delay(2000)
             run = False
     pygame.display.quit()
 
